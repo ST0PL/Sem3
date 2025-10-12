371e592d2977dccf488341631fa45f0e2a3c9baa
st0pl <st0pl0gin@protonmail.com>
2025-10-12 13:11:11 +0700

    Создание перечислений

---
+++ lab4/src/enums/Rank.java
@@ -0,0 +1,23 @@
+package enums;
+
+public enum Rank {
+    PRIVATE, //
+    CORPORAL, // Ефрейтор
+    JUNIOR_SERGEANT, // Младший сержант
+    SERGEANT, // Cержант
+    SENIOR_SERGEANT, // Старший сержант
+    SERGEANT_MAJOR, // Старшина
+    WARRANT_OFFICER, // Прапорщик
+    SENIOR_WARRANT_OFFICER, // Старший прапорщик
+    JUNIOR_LIEUTENANT, // Младший лейтенант
+    LIEUTENANT, // Лейтенант
+    SENIOR_LIEUTENANT, // Старший лейтенант
+    CAPTAIN, // Капитан
+    MAJOR, // Майор
+    LIEUTENAHT_COLONEL, // Подполковник
+    COLONEL, // Полковник
+    MAJOR_GENERAL, // Генерал майор
+    LIEUTENAHT_GENERAL, // Генерал лейтенант
+    COLONEL_GENERAL, // Генерал полковник
+    ARMY_GENERAL // Генерал армии
+}
