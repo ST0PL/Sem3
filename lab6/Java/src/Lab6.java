f3129f592b9b406fd1698400b7cfb08e094b48ff
st0pl <st0pl0gin@protonmail.com>
2025-11-14 18:36:20 +0700

    Применение модификатора protected. Реализация переопределения виртуальной функции

--- lab6/Java/src/Lab6.java
+++ lab6/Java/src/Lab6.java
@@ -1,83 +1,16 @@
 
-import enums.Caliber;
-import enums.Rank;
-import enums.Speciality;
-import enums.SupplyType;
-import enums.UnitType;
-import enums.WarehouseType;
+import enums.LogLevel;
+import java.io.IOException;
 import static java.lang.System.out;
-import java.util.ArrayList;
-import java.util.Arrays;
-import request.SupplyRequestDetail;
-import request.SupplyResponse;
-import unit.Staff;
+import logger.*;
 import unit.Unit;
-import warehouse.Warehouse;
-import warehouse.equipments.Weapon;
-import warehouse.resources.Ammunition;
 
 class Lab6 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Staff[] staff = new Staff[]
-        {
-             new Staff(1, "Иванов Иван Иванович", Rank.PRIVATE, Speciality.MEDIC),
-             new Staff(2, "Морозов Константин Алексеевич", Rank.PRIVATE, Speciality.ENGINEER),
-             new Staff(3, "Васильев Антон Владимирович", Rank.PRIVATE, Speciality.TANKER),
-             new Staff(4, "Кузьмин Вячеслав Александрович", Rank.ARMY_GENERAL, Speciality.TANKER),
-        };
-
-        Unit army = new Unit(1, "1-я Армия", UnitType.ARMY);
-        Unit division = new Unit(2, "1-я Мотострелковая дивизия", UnitType.DIVISION);
-        Unit brigade = new Unit(3, "1-я Мотострелковая бригада", UnitType.BRIGADE);
-        brigade.addSoldiers(new ArrayList<>(Arrays.asList(staff)));
-
-        var searchResult = brigade.findByName("антон");
-        out.println(String.format("Результаты поиска солдатов с именем \"Антон\": %s", searchResult));
-
-        // Попытка создать снаряжение с некорректным колличеством
-
-        try{
-            Weapon corrupted = new Weapon(1, "Самоходная артиллерийская установка 2С1 «Гвоздика»", Caliber.C_122MM, 0);
-        }
-        catch(IllegalArgumentException ex) { out.println(String.format("Обработано исключение при создании снаряжения: %s", ex.getMessage())); }
-        finally { out.println("Выполнен finally блок"); }
-
-        army.assignCommander(staff[3]);
+        FileLogger logger = new FileLogger("logs.txt");
+        logger.Log("Test", LogLevel.WARN, true);
         
-        Warehouse armyWarehouse = new Warehouse(1, "Армейский склад", WarehouseType.REAR);
-        Warehouse divisionWarehouse = new Warehouse(2, "Дивизионный склад", WarehouseType.FIELD);
-
-
-        armyWarehouse.addEquipment(new Weapon(1, "Самоходная артиллерийская установка 2С1 «Гвоздика»", Caliber.C_122MM, 20));
-        armyWarehouse.addResource(new Ammunition(1, "Патроны 5.45мм", Caliber.C_545MM, 2000));
-        divisionWarehouse.addResource(new Ammunition(1, "Патроны 5.45мм", Caliber.C_545MM, 100));
-
-        army.assignWarehouse(armyWarehouse);
-        division.assignWarehouse(divisionWarehouse);
-
-        army.addChildUnit(division);
-        division.addChildUnit(brigade);
-
-        out.println(String.format("Создана иерархия:"));
-
-
-        printTree(army, "");
-
-
-        out.println("\nБригада запрашивает 2200 патронов и 25 артиллерийских установок калибра 122мм (на складе дивизии только 100 патронов, на армейском 2000 патронов и 20 артиллерийских установок):");
-
-
-        SupplyRequestDetail[] details = new SupplyRequestDetail[]{
-            new SupplyRequestDetail(1, SupplyType.AMMUNITION, 2200).withCaliber(Caliber.C_545MM),
-            new SupplyRequestDetail(2, SupplyType.WEAPON, 25).withCaliber(Caliber.C_122MM)
-        };
-
-        var request = brigade.createRequest(new ArrayList<>(Arrays.asList(details)));
-        SupplyResponse response = division.makeSupplyRequest(request);
-        out.println("\nРезультат\n");
-        out.println("Статус: "+SupplyResponse.statusToString(response.getStatus()));
-        out.println("Комментарий: " + response.getComment());
     }
 
     public static void printTree(Unit parent, String tabs){
---
+++ lab6/Java/src/logger/FileLogger.java
@@ -0,0 +1,27 @@
+package logger;
+import enums.LogLevel;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+public class FileLogger extends AdvancedLogger{
+
+    private Path path;
+
+    public Path getPath(){
+        return path;
+    }
+
+    public FileLogger(String path){
+        this.path = Paths.get(path);
+    }
+
+    public void Log(String text, LogLevel level, Boolean print) throws IOException{
+        String formatted = String.format("[%s][%s] %s", GetNowTime(), levels.get(level), text);
+        if(print){
+            Log(text, level);
+        }
+        Files.writeString(path, (Files.exists(path) && Files.size(path) > 0 ? System.lineSeparator() : "") + formatted, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    }
+}
