32a77f6b5e8520ff6312bb865eea6397efdfa468
st0pl <st0pl0gin@protonmail.com>
2025-11-16 11:07:48 +0700

    Создание макета главного окна

--- CourseProject/ILS_WPF/MainWindow.xaml.cs
+++ CourseProject/ILS_WPF/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ILS_WPF
 {
@@ -19,6 +11,20 @@ namespace ILS_WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
