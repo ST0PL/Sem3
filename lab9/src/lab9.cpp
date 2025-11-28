dec5b4f38fb4328e5c060a37219cc717f0be349d
st0pl <st0pl0gin@protonmail.com>
2025-11-29 06:24:21 +0700

    Реализация внешней функции, вычисляемой на этапе компиляции

--- lab9/src/lab9.cpp
+++ lab9/src/lab9.cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
+#include <string>
+
+
+
+consteval int Power(int z, int x) {
+    int result = 1;
+    for (int i = 0; i < x; i++)
+        result *= z;
+    return result;
+}
+
 
 int main()
 {
-
+    constexpr int a = Power(2,10);
+    std::cout << "Результат вычисления power(2,10) во время компиляции: " << a << std::endl;
 }
