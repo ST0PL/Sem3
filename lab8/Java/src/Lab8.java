a1e231132b24c876106bfb28d105b09d898861b4
st0pl <st0pl0gin@protonmail.com>
2025-11-28 19:59:33 +0700

    Реализация обобщенного метода, не привязанного к экземпляру класса

--- lab8/Java/src/Lab8.java
+++ lab8/Java/src/Lab8.java
@@ -1,14 +1,10 @@
-import enums.LogLevel;
 import java.io.IOException;
-import logger.*;
 
 public class Lab8 {
     public static void main(String[] args) throws IOException {
-        Logger logger = new Logger();
-        logger.RegisterLogger(new FileLogger("logs.txt"));
-        logger.RegisterLogger(new ConsoleLogger());
-        logger.Log("Демонстрация мульти-логгера", LogLevel.INFO);
-        logger.RemoveLogger(logger.GetWrappers().get(1).getId());
-        logger.Log("Сообщение после удаления консольного логгера", LogLevel.WARN);
+
+    }
+    public static <T extends Number> double CalculateRequirement(T count, T perOne) {
+        return count.doubleValue()*perOne.doubleValue();
     }
 }
